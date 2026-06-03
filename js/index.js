eb3717a9cace9bb9f38b18c3463bae0c0691e4b6
Kennedi1z <kennediwilson@sbcglobal.net>
2026-06-02 21:33:27 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -19,4 +19,30 @@ for ( let i = 0; i < skills.length; i++) {
     skillsList.appendChild(skill);
 }
 
+const messageForm = document.forms["leave_message"]; 
+messageForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    console.log(usersName);
+    console.log(usersEmail);
+    console.log(usersMessage);
+
+    const messageSection = document.querySelector('#Messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a><span> wrote: ${usersMessage}</span>`;
+    const removeButton = document.createElement('button');  
+    removeButton.innerText = 'Remove';
+    removeButton.addEventListener('click', function() {
+        const entry = removeButton.parentNode;
+        entry.removeChild();
+        });
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+    });
+    messageForm.reset();
+
 
